611e6d5b8a3f614b99df6f52113d91bb96e8b57f
Leonardo <leomaben@hotmail.com>
2025-03-21 12:24:48 +0100

    Added a dataprocessing class for handling loading and prep of data

--- Models/LinearRegression.py
+++ Models/LinearRegression.py
@@ -1,4 +1,3 @@
-import pandas
 import pandas as pd
 from typing import List
 from sklearn.linear_model import LinearRegression
@@ -6,52 +5,65 @@ from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 import numpy as np
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-def prepareTrainData(train_df:pandas.DataFrame, drop_coloumns:List[str]):
-    train_df = train_df.drop(drop_coloumns, axis=1)
-    train_df['RUL'] = train_df.groupby('unit number')['time cycles'].transform(max) - train_df['time cycles']
-    train_df = train_df.drop(['unit number', 'time cycles'], axis=1)
 
-    scaler = StandardScaler()
+class DataProcessing:
+    def __init__(self, train_path:str, test_path: str):
+        self.train_df = pd.read_csv(train_path)
+        test_df = pd.read_csv(test_path)
+        test_df = test_df.groupby('unit number').last().reset_index()
+        self.test_df = test_df
 
-    X = train_df.drop('RUL', axis=1)
-    y = train_df['RUL'].clip(upper=125)
+    def __preprocessData(self, df, drop_columns) -> pd.DataFrame :
 
-    X_scaled = scaler.fit_transform(X)
+        df = df.drop(drop_columns, axis=1)
+        df['RUL'] = df.groupby('unit number')['time cycles'].transform(max) - df['time cycles']
+        df = df.drop(['unit number', 'time cycles'], axis=1)
 
+        return df
 
-    return X_scaled, y
+    def prepareData(self, drop_columns: List[str]):
 
+        train_df = self.__preprocessData(self.train_df, drop_columns)
+        test_df = self.__preprocessData(self.test_df, drop_columns)
 
-def evaluate(y, y_predictions):
-    mae = mean_absolute_error(y, y_predictions)
-    rmse = np.sqrt(mean_squared_error(y, y_predictions))
-    r2 = r2_score(y, y_predictions)
+        scaler = StandardScaler()
 
+        X_train = train_df.drop('RUL', axis=1)
+        y_train = train_df['RUL'].clip(upper=125)
 
-    print(f'Mean Absolute Error is: {mae}\n'
-          f'Mean Squared Error is: {rmse}\n'
-          f'R2 Score is: {r2}')
+        X_test = test_df.drop('RUL', axis=1)
 
+        X_scaled_train = scaler.fit_transform(X_train)
+        X_scaled_test = scaler.fit_transform(X_test)
+
+        return X_scaled_train, y_train, X_scaled_test
+
+    def evaluate(self, y, y_predictions):
+        mae = mean_absolute_error(y, y_predictions)
+        rmse = np.sqrt(mean_squared_error(y, y_predictions))
+        r2 = r2_score(y, y_predictions)
+
+        print(f'Mean Absolute Error is: {mae}\n'
+              f'Mean Squared Error is: {rmse}\n'
+              f'R2 Score is: {r2}')
 
 def main():
     train_path = '../clean_data/train_FD001.csv'
     test_path ='../clean_data/test_FD001.csv'
-    train_df = pd.read_csv(train_path)
-    test_df = pd.read_csv(test_path)
+
     y_test = pd.read_csv('../CMAPSSData/RUL_FD001.txt', names=['RUL'])
 
+    drop_columns = ['settings 1', 'settings 2', 'settings 3', 'sensor 1'
+                    , 'sensor 5', 'sensor 6', 'sensor 10', 'sensor 16'
+                    , 'sensor 18', 'sensor 19']
 
-    drop_coloumns = ['settings 1', 'settings 2', 'settings 3', 'sensor 1',  'sensor 5', 'sensor 6', 'sensor 10',
-                     'sensor 16', 'sensor 18', 'sensor 19']
+    data_processor = DataProcessing(train_path, test_path)
+    X_train, y_train, X_test = data_processor.prepareData(drop_columns)
 
-    X_train_scaled, y_train = prepareTrainData(train_df, drop_coloumns)
-
-    X_test = test_df.groupby('unit number').last().reset_index()
-    X_test_scaled, _ = prepareTrainData(X_test, drop_coloumns)
 
     poly = PolynomialFeatures(3)
-    X_train = poly.fit_transform(X_train_scaled)
-    X_test = poly.fit_transform(X_test_scaled)
+    X_train = poly.fit_transform(X_train)
+    X_test = poly.fit_transform(X_test)
 
 
     model = LinearRegression()
@@ -62,11 +74,9 @@ def main():
     y_test_predicitions = model.predict(X_test)
 
     print('Training Data: ')
-    evaluate(y_train, y_train_predictions)
+    data_processor.evaluate(y_train, y_train_predictions)
     print('-------------------------\nTest Data: ')
-    evaluate(y_test, y_test_predicitions)
-
-
+    data_processor.evaluate(y_test, y_test_predicitions)
 
 
 
